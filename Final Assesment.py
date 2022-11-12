da223a33bc492c8c38bfc04638c905f45d94f6bf
TurboNeso <117675202+TurboNeso@users.noreply.github.com>
2022-11-12 09:57:38 +0100

    Add files via upload

---
+++ Final Assesment.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 1. My Jupyter Notebook on IBM Watson Studio
+
+# **Nasraddin Gozalli** 
+# 
+#  Direct Operation Cost Controller
+
+# *I am interested in data science because it is facinating to be able to find patterns for whole population with using small sample size. Moreover, you understand the application maths in real life.*
+
+# ### My code below is a for loop and will print even numbers up to 20
+
+# In[5]:
+
+
+for x in range(2,21,2):
+    print(x)
+
+
+#  - bullet list
+#      - subbullet
+#  1. numbered list
+#  2. numbered list
+#      1. Subline
+#      2. Subline
+#  
+#  |name||age||Salary|
+#  |----||---||------|
+#  |Neso||28||7500|
+
+# In[ ]:
+
+
+
+
